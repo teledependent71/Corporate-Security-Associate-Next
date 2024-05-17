ac6ac7ee3d02a19a8a2ff571b23128c4d94c5164
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-05-17 20:31:32 +0300

    Commit from TeleportHQ

---
+++ pages/test-page.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import { getEntityByAttribute } from '@teleporthq/cms-mappers/caisy'
+
+const TestPage = (props) => {
+  return (
+    <>
+      <div className="test-page-container">
+        <Head>
+          <title>test-page - Corporate Security Associate</title>
+          <meta
+            property="og:title"
+            content="test-page - Corporate Security Associate"
+          />
+        </Head>
+        <DataProvider
+          renderSuccess={(context_zffceq) => (
+            <>
+              <h1 id={context_zffceq?.id}>Heading</h1>
+            </>
+          )}
+          params={{
+            projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+            query:
+              'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+            attribute: 'id',
+            id: '2',
+          }}
+          initialData={props.contextZffceqProp}
+          persistDataDuringLoading={true}
+        />
+      </div>
+      <style jsx>
+        {`
+          .test-page-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default TestPage
+
+export async function getStaticProps(context) {
+  try {
+    const contextZffceqProp = await getEntityByAttribute({
+      ...context?.params,
+      projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+      query:
+        'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+      attribute: 'id',
+      id: '2',
+    })
+    return {
+      props: {
+        contextZffceqProp: contextZffceqProp?.data?.[0],
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      props: {},
+      revalidate: 60,
+    }
+  }
+}
